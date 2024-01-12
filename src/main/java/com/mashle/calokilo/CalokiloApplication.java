b20536a189bdedbd02d3d408a92464607a8ef50d
yeanci <myemail>
2024-01-12 23:56:00 +0100

    chore: initialize spring boot project

---
+++ src/main/java/com/mashle/calokilo/CalokiloApplication.java
@@ -0,0 +1,13 @@
+package com.mashle.calokilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalokiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalokiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashle/calokilo/CalokiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashle.calokilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalokiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
