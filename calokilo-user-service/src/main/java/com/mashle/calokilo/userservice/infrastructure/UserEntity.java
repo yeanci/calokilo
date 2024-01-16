285f9953f0e0c0b3f9285e64973494058d2ba73e
yeanci <myemail>
2024-01-16 22:31:41 +0100

    test(user) : test infrastructure user entity

---
+++ calokilo-user-service/src/main/java/com/mashle/calokilo/userservice/infrastructure/UserEntity.java
@@ -0,0 +1,53 @@
+package com.mashle.calokilo.userservice.infrastructure;
+
+import com.mashle.calokilo.userservice.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "height")
+    private int height;
+
+
+    public UserEntity(User user) {
+        this.firstName = user.firstName();
+        this.email = user.email();
+        this.password = user.password();
+        this.birthDate = user.birthDate();
+        this.height = user.height();
+    }
+
+
+    public User toCreatedUser() {
+        return User.builder()
+                .id(this.id)
+                .firstName(this.firstName)
+                .email(this.email)
+                .password(password)
+                .birthDate(this.birthDate)
+                .height(this.height)
+                .build();
+    }
+}
---
+++ calokilo-user-service/src/test/java/com/mashle/calokilo/userservice/ut/UserEntityTest.java
@@ -0,0 +1,34 @@
+package com.mashle.calokilo.userservice.ut;
+
+import com.mashle.calokilo.userservice.domain.User;
+import com.mashle.calokilo.userservice.infrastructure.UserEntity;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserEntityTest {
+
+    @Test
+    void shouldMapUserEntityToUser() {
+        // Given
+        UserEntity userEntity = new UserEntity(1L,
+                "John",
+                "john@gmail.com",
+                "mySecretP@ssword",
+                LocalDate.of(1980, 12, 1),
+                180);
+
+        // When
+        User user = userEntity.toCreatedUser();
+
+        // Then
+        assertThat(user.id()).isEqualTo(1L);
+        assertThat(user.firstName()).isEqualTo("John");
+        assertThat(user.email()).isEqualTo("john@gmail.com");
+        assertThat(user.password()).isEqualTo("mySecretP@ssword");
+        assertThat(user.birthDate()).isEqualTo(LocalDate.of(1980, 12, 1));
+        assertThat(user.height()).isEqualTo(180);
+    }
+}
