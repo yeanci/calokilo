0b02af820443e4888351bedf1477a7d73ae535ab
yeanci <myemail>
2024-01-17 13:47:26 +0100

    chore: simplify services name

--- user-service/src/main/java/com/mashle/calokilo/userservice/application/controllers/UserController.java
+++ user-service/src/main/java/com/mashle/calokilo/userservice/application/controllers/UserController.java
@@ -2,8 +2,10 @@ package com.mashle.calokilo.userservice.application.controllers;
 
 import com.mashle.calokilo.userservice.domain.ports.CreateUserPort;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,10 @@ public class UserController {
     private CreateUserPort createUserPort;
 
     @PostMapping
-    public ResponseEntity<UserResource> createUser(CreateUserRequest userRequest) {
+    public ResponseEntity<UserResource> createUser(@RequestBody CreateUserRequest userRequest) {
         UserResource saved = UserResource.from(createUserPort.createUser(userRequest.toUser()));
 
-        return ResponseEntity.ok().body(saved);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
 }
---
+++ user-service/src/test/java/com/mashle/calokilo/userservice/it/UserControllerTest.java
@@ -0,0 +1,95 @@
+package com.mashle.calokilo.userservice.it;
+
+import com.mashle.calokilo.userservice.application.controllers.UserResource;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+public class UserControllerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1");
+
+    @Autowired
+    WebTestClient webTestClient;
+
+
+    @Test
+    void connectionEstablished() {
+        assertThat(postgres.isCreated()).isTrue();
+        assertThat(postgres.isRunning()).isTrue();
+    }
+
+    @Test
+    @Rollback
+    public void shouldCreateUser_whenValidData_thenCreateUser() {
+        // Given
+        String validUser = """
+                    {
+                        "firstName": "John",
+                        "email" : "john@gmail.com",
+                        "password": "mySecretP@ssword",
+                        "birthDate" : "1980-12-01",
+                        "height": 180
+                    }
+                """;
+
+        // When
+        WebTestClient.ResponseSpec response = webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(validUser))
+                .exchange();
+
+        // Then
+        response
+                .expectStatus().isCreated()
+                .expectBody()
+                    .jsonPath("$.id").isEqualTo(1L)
+                    .jsonPath("$.firstName").isEqualTo("John")
+                    .jsonPath("$.email").isEqualTo("john@gmail.com")
+                    .jsonPath("$.password").doesNotExist()
+                    .jsonPath("$.birthDate").isEqualTo("1980-12-01")
+                    .jsonPath("$.height").isEqualTo(180);
+    }
+
+    @Test
+    @Rollback
+    public void shouldCreateUser_whenNotValidData_thenReturnError() {
+        // Given
+        String invalidUser = """
+                    {
+                        "firstName": "J",
+                        "email" : "john@gmail",
+                        "password": "mySecret",
+                        "birthDate" : "2040-12-01",
+                        "height": -15
+                    }
+                """;
+
+        // When
+        WebTestClient.ResponseSpec response = webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(invalidUser))
+                .exchange();
+
+        // Then
+        response.expectStatus().is5xxServerError();
+    }
+}
